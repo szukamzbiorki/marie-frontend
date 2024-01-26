be1027044aa7b0abfdff6aabf2aa3ca124ac32c3
Bartek Pierściński <bartoszjpierscinski@gmail.com>
2024-01-26 12:48:58 +0100

    dsa

--- pages/index.vue
+++ pages/index.vue
@@ -4,18 +4,16 @@
     <!-- {{ idle }} {{ idledFor }} -->
     <div class="grid">
       <div v-for="(p, i) in data.projects" class="item">
-        <div class="top">
-          <div class="ani">
-            <div class="title">{{ p.title }}<span>,&nbsp;</span></div>
-            <div class="year">{{ p.year }}</div>
-            <div class="client">{{ p.client }}</div>
-          </div>
-          <div class="desc">{{ p.description }}</div>
+        <div class="ani">
+          <div class="title">{{ p.title }}<span>,&nbsp;</span></div>
+          <div class="year">{{ p.year }}</div>
+          <div class="client">{{ p.client }}</div>
         </div>
-        <div class="bot">
+        <div class="carousel">
           <LazyCarousel :content="p"></LazyCarousel>
           <div class="gradient"></div>
         </div>
+        <div class="desc">{{ p.description }}</div>
       </div>
     </div>
   </div>
@@ -53,101 +51,92 @@
   .item {
     display: grid;
     grid-template-columns: repeat(12, 1fr);
-    gap: 0 var(--space-m);
+    gap: var(--space-s) var(--space-m);
     grid-template-rows: auto auto;
     background-color: white;
 
-    & > .top {
-      display: grid;
-      grid-template-columns: repeat(12, 1fr);
-      grid-template-rows: auto 0fr;
+    grid-template-rows: auto auto 0fr;
+    grid-column: 1/-1;
+    grid-row: 1/2;
+    cursor: default;
+    transition: grid-template-rows 0.5s ease-in-out;
+    width: calc(100vw - 2 * (var(--space-m)));
+
+    & > .ani {
       grid-column: 1/-1;
-      grid-row: 1/2;
-      cursor: default;
-      transition: grid-template-rows 0.5s ease-in-out;
-      width: calc(100vw - 2 * (var(--space-m)));
-
-      & > .ani {
-        grid-column: 1/-1;
-        display: flex;
-        flex-direction: row;
-        @media screen and (max-width: 600px) {
-          width: 100%;
-          justify-content: space-between;
-          & > .title {
-            & > span {
-              display: none;
-            }
-          }
-        }
+      display: flex;
+      flex-direction: row;
+      @media screen and (max-width: 600px) {
+        width: 100%;
+        justify-content: space-between;
         & > .title {
-          transition: min-width 0.5s ease-in-out;
-          min-width: 3px;
           & > span {
-            transition: opacity 0.5s ease-in-out;
+            display: none;
           }
         }
-
-        & > .year {
-          transition: min-width 0.5s ease-in-out;
-          min-width: 3px;
-        }
-
-        & > .client {
-          transition: min-width 0.5s ease-in-out, opacity 0.5s ease-in-out;
-          min-width: 3px;
-          opacity: 0;
+      }
+      & > .title {
+        transition: min-width 0.5s ease-in-out;
+        min-width: 3px;
+        & > span {
+          transition: opacity 0.5s ease-in-out;
         }
       }
 
-      & > .desc {
-        grid-column: 1 / -1;
-        width: calc((100vw - 2 * var(--space-m)) / 12 * 3);
-        grid-row: 2/-1;
-        overflow: hidden;
-        color: grey;
-        /* font-family: 'NHG', sans-serif; */
-        white-space: pre-wrap;
+      & > .year {
+        transition: min-width 0.5s ease-in-out;
+        min-width: 3px;
+      }
+
+      & > .client {
+        transition: min-width 0.5s ease-in-out, opacity 0.5s ease-in-out;
+        min-width: 3px;
+        opacity: 0;
       }
     }
+    & > .desc {
+      grid-column: 1 / -1;
+      width: calc((100vw - 2 * var(--space-m)) / 12 * 3);
+      grid-row: 3/-1;
+      overflow: hidden;
+      color: grey;
+      /* font-family: 'NHG', sans-serif; */
+      white-space: pre-wrap;
+    }
     @media screen and (max-width: 600px) {
-      & > .top {
-        grid-template-rows: auto 1fr;
-        & > .desc {
-          width: 100%;
-        }
+      grid-template-rows: auto auto 1fr;
+      & > .desc {
+        width: calc((100vw - 2 * var(--space-m)));
       }
     }
     @media (hover: hover) {
       &:hover {
-        & > .top {
-          grid-template-rows: auto 1fr;
+        grid-template-rows: auto auto 1fr;
 
-          & > .ani {
-            display: flex;
-            flex-direction: row;
+        & > .ani {
+          display: flex;
+          flex-direction: row;
 
-            & > .title {
-              min-width: calc((100vw - 2 * var(--space-m)) / 12 * 3);
-              & > span {
-                opacity: 0;
-              }
+          & > .title {
+            min-width: calc((100vw - 2 * var(--space-m)) / 12 * 3);
+            & > span {
+              opacity: 0;
             }
-            & > .year {
-              min-width: calc((100vw - 2 * var(--space-m)) / 12 * 1);
-            }
+          }
+          & > .year {
+            min-width: calc((100vw - 2 * var(--space-m)) / 12 * 1);
+          }
 
-            & > .client {
-              min-width: calc((100vw - 2 * var(--space-m)) / 12 * 3);
-              opacity: 1;
-            }
+          & > .client {
+            min-width: calc((100vw - 2 * var(--space-m)) / 12 * 3);
+            opacity: 1;
           }
         }
       }
     }
   }
 
-  .bot {
+  .carousel {
     position: relative;
     width: calc(100vw - 2 * (var(--space-m)));
     & > .gradient {
