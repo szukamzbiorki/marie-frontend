cda2dfb5876be1c6c1e805f6807271376c59b37d
Bartek Pierściński <bartoszjpierscinski@gmail.com>
2024-01-31 15:21:14 +0100

    dsa

--- pages/about.vue
+++ pages/about.vue
@@ -1,142 +1,142 @@
 <template>
-  <div class="wrapper">
-    <div class="info">
-      <NuxtLink to="/" class="back">Back</NuxtLink>
-      <div class="content">
-        <a class="email" :href="`mailto:${data.about.email}`">{{
-          data.about.email
-        }}</a>
-        <a class="phone" :href="`tel:${data.about.phone}`">{{
-          data.about.phone
-        }}</a>
-        <div class="description">
-          {{ data.about.about }}
-          <div @click="credits = true" v-if="!credits" class="credits">
-            Credits
-          </div>
-          <div v-if="credits" class="credits">
-            Code: <a href="https://bartekp.info">Bartek Pierściński</a
-            ><br />Typeface:
-            <a href="https://j-ltf.com">Jung-Lee Type Foundry</a>
-          </div>
-        </div>
-        <Media class="image" :medium="data.about.image"></Media>
-      </div>
-    </div>
-  </div>
+	<div class="wrapper">
+		<div class="info">
+			<NuxtLink to="/" class="back">Back</NuxtLink>
+			<div class="content">
+				<a class="email" :href="`mailto:${data.about.email}`">{{
+					data.about.email
+				}}</a>
+				<a class="phone" :href="`tel:${data.about.phone}`">{{
+					data.about.phone
+				}}</a>
+				<div class="description">
+					{{ data.about.about }}
+					<div @click="credits = true" v-if="!credits" class="credits">
+						Credits
+					</div>
+					<div v-if="credits" class="credits">
+						Code: <a href="https://bartekp.info">Bartek Pierściński</a
+						><br />Typeface:
+						<a href="https://j-ltf.com">Jung-Lee Type Foundry</a>
+					</div>
+				</div>
+				<Media class="image" :medium="data.about.image"></Media>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script setup>
-  const { mobile } = useScreenSize()
-  const credits = ref(false)
+	const { mobile } = useScreenSize()
+	const credits = ref(false)
 
-  const query = groq`{
+	const query = groq`{
     "about": *[_type == "about"][0],
     }`
-  const sanity = useSanity()
-  const { data } = await useAsyncData(() => sanity.fetch(query))
+	const sanity = useSanity()
+	const { data } = await useAsyncData(() => sanity.fetch(query))
 
-  definePageMeta({
-    layout: 'default',
-  })
+	definePageMeta({
+		layout: 'default',
+	})
 
-  useHead({
-    title: 'About',
-  })
-  useSeoMeta({})
+	useHead({
+		title: 'About',
+	})
+	useSeoMeta({})
 </script>
 
 <style scoped>
-  .grid {
-    margin-top: 10vh;
-    padding: var(--space-m);
-    background-color: var(--color);
-    display: flex;
-    flex-direction: column;
-    gap: var(--space-m);
-  }
+	.grid {
+		margin-top: 10vh;
+		padding: var(--space-m);
+		background-color: var(--color);
+		display: flex;
+		flex-direction: column;
+		gap: var(--space-m);
+	}
 
-  .item {
-    display: grid;
-    grid-template-columns: repeat(12, 1fr);
-    gap: var(--space-m);
-    grid-template-rows: auto auto;
-    background-color: white;
-  }
+	.item {
+		display: grid;
+		grid-template-columns: repeat(12, 1fr);
+		gap: var(--space-m);
+		grid-template-rows: auto auto;
+		background-color: white;
+	}
 
-  .textbox {
-    display: grid;
-    grid-template-columns: repeat(12, 1fr);
-    grid-column: 1/-1;
-    grid-row: 1/2;
-  }
+	.textbox {
+		display: grid;
+		grid-template-columns: repeat(12, 1fr);
+		grid-column: 1/-1;
+		grid-row: 1/2;
+	}
 
-  .titel {
-    grid-column: span 4;
-  }
+	.titel {
+		grid-column: span 4;
+	}
 
-  .image {
-    grid-column: 1/7;
-    grid-row: 2/-1;
-  }
+	.image {
+		grid-column: 1/7;
+		grid-row: 2/-1;
+	}
 
-  .info {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100vw;
-    height: 100vh;
-    background-color: white;
-    z-index: 2;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    padding: var(--space-m);
-    @media screen and (max-width: 600px) {
-      justify-content: flex-start;
-      gap: 100px;
-    }
-  }
+	.info {
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100vw;
+		height: 100vh;
+		background-color: white;
+		z-index: 2;
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		padding: var(--space-m);
+		@media screen and (max-width: 600px) {
+			justify-content: flex-start;
+			gap: 100px;
+		}
+	}
 
-  .content {
-    display: grid;
-    grid-template-columns: repeat(12, 1fr);
-    gap: var(--space-m);
-    grid-template-rows: auto auto;
-    @media screen and (max-width: 600px) {
-      grid-template-columns: repeat(2, 1fr);
-    }
-  }
+	.content {
+		display: grid;
+		grid-template-columns: repeat(12, 1fr);
+		gap: var(--space-m);
+		grid-template-rows: auto auto;
+		@media screen and (max-width: 600px) {
+			grid-template-columns: repeat(2, 1fr);
+		}
+	}
 
-  .content > div,
-  a {
-    grid-column: span 3;
-    @media screen and (max-width: 600px) {
-      grid-column: span 1;
-    }
-  }
+	.content > div,
+	a {
+		grid-column: span 3;
+		@media screen and (max-width: 600px) {
+			grid-column: span 1;
+		}
+	}
 
-  .description {
-    grid-column: 1 / span 6 !important;
-    @media screen and (max-width: 600px) {
-      grid-column: span 2 !important;
-    }
-    white-space: pre-wrap;
-  }
+	.description {
+		grid-column: 1 / span 6 !important;
+		@media screen and (max-width: 600px) {
+			grid-column: span 2 !important;
+		}
+		white-space: pre-wrap;
+	}
 
-  .image {
-    grid-column: span 6 !important;
-    @media screen and (max-width: 600px) {
-      grid-column: span 2 !important;
-    }
-    grid-row: 1 / span 1;
-    white-space: pre-wrap;
-  }
+	.image {
+		grid-column: span 6 !important;
+		@media screen and (max-width: 600px) {
+			grid-column: span 2 !important;
+		}
+		grid-row: 1 / span 1;
+		white-space: pre-wrap;
+	}
 
-  .email {
-    grid-column: 1 / span 3 !important;
-    @media screen and (max-width: 600px) {
-      grid-column: 1 / span 1 !important;
-    }
-  }
+	.email {
+		grid-column: 1 / span 3 !important;
+		@media screen and (max-width: 600px) {
+			grid-column: 1 / span 1 !important;
+		}
+	}
 </style>
