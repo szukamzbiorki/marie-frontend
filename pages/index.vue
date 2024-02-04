a13ce2903ed05acd8610efa6036285841d6b4056
Bartek Pierściński <bartoszjpierscinski@gmail.com>
2024-02-04 12:18:16 +0100

    das

--- pages/index.vue
+++ pages/index.vue
@@ -1,32 +1,32 @@
 <template>
-  <div class="wrapper">
-    <LayoutHeader></LayoutHeader>
-    <div class="grid">
-      <Project :p="p" v-for="(p, i) in data.projects" class="item"> </Project>
-    </div>
-  </div>
+	<div class="wrapper">
+		<LayoutHeader></LayoutHeader>
+		<div class="grid">
+			<Project :p="p" v-for="(p, i) in data.projects" class="item"> </Project>
+		</div>
+	</div>
 </template>
 
 <script setup>
-  const query = groq`{
+	const query = groq`{
     "projects": *[_type == "project" && show == true]| order(orderRank),
     }`
-  const sanity = useSanity()
-  const { data } = await useAsyncData(() => sanity.fetch(query))
+	const sanity = useSanity()
+	const { data } = await useAsyncData(() => sanity.fetch(query))
 
-  definePageMeta({
-    layout: 'default',
-  })
+	definePageMeta({
+		layout: 'default',
+	})
 
-  new BugController({ minBugs: 4, maxBugs: 6, mouseOver: 'fly' })
+	new BugController({ minBugs: 1, maxBugs: 1, mouseOver: 'fly' })
 
-  useHead({})
-  useSeoMeta({})
+	useHead({})
+	useSeoMeta({})
 </script>
 
 <style lang="postcss">
-  .wrapper {
-    /* background-image: linear-gradient(
+	.wrapper {
+		/* background-image: linear-gradient(
         45deg,
         var(--blueski) 25%,
         transparent 25%
@@ -36,17 +36,17 @@
       linear-gradient(-45deg, transparent 75%, #808080 75%);
     background-size: 10vw 30vw;
     background-position: 0 0, 0 10px, 10px -10px, -10px 0px; */
-    /* mask-image: linear-gradient(90deg, rgb(0 0 0 / 100%), 80%, transparent); */
-    background-color: var(--blueski);
-    /* mask-mode: alpha; */
-  }
-  .grid {
-    margin-top: 10vh;
-    padding: var(--space-m);
-    background-color: var(--color);
-    display: flex;
-    flex-direction: column;
-    gap: var(--space-m);
-    overflow: hidden;
-  }
+		/* mask-image: linear-gradient(90deg, rgb(0 0 0 / 100%), 80%, transparent); */
+		background-color: var(--blueski);
+		/* mask-mode: alpha; */
+	}
+	.grid {
+		margin-top: 10vh;
+		padding: var(--space-m);
+		background-color: var(--color);
+		display: flex;
+		flex-direction: column;
+		gap: var(--space-m);
+		overflow: hidden;
+	}
 </style>
